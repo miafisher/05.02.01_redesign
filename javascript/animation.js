a365154b8c881732f727b4d1c33e9f0f5e26a76e
“Mia <mb.mia.bosse@gmail.com>
2022-05-25 11:42:20 +0200

    burgermenu start

--- javascript/animation.js
+++ javascript/animation.js
@@ -10,3 +10,28 @@ function myFunction() {
     document.getElementById("flag").src = "usa.svg";
   }
 }
+
+// burgermenu
+// Lav variablen "btn", der henviser til ".toggle-btn"
+const btn = document.querySelector(".toggle-btn");
+// Lav variablen "menu", der henviser til ".main-menu"
+const menu = document.querySelector(".main-menu");
+// Lav funktionen "toggleMenu()" med følgende funktionalitet
+function toggleMenu() {
+  // a) toggle klassen "shown" på menu vha. classList.toggle
+  menu.classList.toggle("shown");
+  // b) lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
+  const menuShown = menu.classList.contains("shown");
+  // c) spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
+  if (menuShown) {
+    console.log(menuShown); // se i konsollen
+    btn.textContent = "Luk"; // sæt btn.textContent til "Luk", hvis menuShown er "true"
+  } else {
+    console.log(menuShown); // se i konsollen
+    btn.textContent = "Menu"; // sæt btn.textContent til "Menu", hvis menuShown er "false"
+  }
+  // "toggleMenu()" slutter her
+}
+
+// Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
+btn.addEventListener("click", toggleMenu);
