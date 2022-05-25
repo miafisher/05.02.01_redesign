b40cc98daa7675422fae3d9d09e8a04425e3d272
Mads <mschouolesen@gmail.com>
2022-05-25 10:09:22 +0200

    nye flag

--- javascript/animation.js
+++ javascript/animation.js
@@ -2,9 +2,11 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("flag-mode");
 
-  if (document.getElementById("flag").src == "english.svg") {
+  if (document.getElementById("flag").src == "usa.svg") {
     document.getElementById("flag").src = "danish.svg";
   } else {
-    document.getElementById("flag").src = "english.svg";
+    document.getElementById("flag").src = "danish.svg";
+
+    document.getElementById("flag").src = "usa.svg";
   }
 }
