6002f1bb03fc18b693a39704a7cc74d50ba3a767
Mads <mschouolesen@gmail.com>
2022-05-25 11:37:55 +0200

    flag uden border og typografi til h1

--- javascript/animation.js
+++ javascript/animation.js
@@ -2,10 +2,10 @@ function myFunction() {
   var element = document.body;
   element.classList.toggle("flag-mode");
 
-  if (document.getElementById("flag").src == "usa.svg") {
+  if (document.getElementById("flag").src == "danish.svg") {
     document.getElementById("flag").src = "danish.svg";
   } else {
-    document.getElementById("flag").src = "danish.svg";
+    document.getElementById("flag").src = "usa.svg";
 
     document.getElementById("flag").src = "usa.svg";
   }
